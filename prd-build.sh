f928147bf2bc58ec9962f27b0af7b3e937f846cb
vhzkclq0705 <vhzkclq0705@naver.com>
2025-01-20 14:09:14 +0900

    버전 업데이트

--- prd-build.sh
+++ prd-build.sh
@@ -2,6 +2,6 @@
 pnpm docs:build
 cp CNAME docs/
 cp robots.txt docs/
-# cp naver10ed219ad8cc83978ef28cef5bbcaf2f.html docs/
+cp naver10ed219ad8cc83978ef28cef5bbcaf2f.html docs/
 git add .
 git commit -m "build: update docs"
