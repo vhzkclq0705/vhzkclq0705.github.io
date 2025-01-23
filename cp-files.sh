4c7ee49d6a010158bc2c0412d9748a0ae9df7ff4
vhzkclq0705 <vhzkclq0705@naver.com>
2025-01-23 10:22:49 +0900

    구글 소유주 확인

--- cp-files.sh
+++ cp-files.sh
@@ -5,5 +5,6 @@ date
 timedatectl
 cp CNAME docs/
 cp robots.txt docs/
-cp naver10ed219ad8cc83978ef28cef5bbcaf2f.html docs/
+# cp naver10ed219ad8cc83978ef28cef5bbcaf2f.html docs/
+cp googlef64c31af1226be5b.html
 cat /etc/os-release
